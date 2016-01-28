16bbc7c0938aa5fc45d4011fbf66c798596431a4
Kyle <Kyle.Uhan@gmail.com>
2016-01-27 22:53:19 -0600

    clickable

--- script.js
+++ script.js
@@ -17,47 +17,52 @@
 			col--;
 		}
 
-		var letter = String.fromCharCode(row);	
-		
+		var letter = String.fromCharCode(row);
+
 		if (even) {
 			addedClass = addedClass + " whiteSquare";
 		} else {
 			addedClass = addedClass + " blackSquare";
 		}
 		piece.setAttribute('class', 'piece');
-		piece.addEventListener("click", handleTileClicked);
-		if(col === 1 || col === 8){
-			if(row === 65 || row === 72){
+
+
+		//piece.addEventListener("click", handlePieceClicked);
+
+
+		if (col === 1 || col === 8) {
+			if (row === 65 || row === 72) {
 				pieceClasses = pieceClasses + "tower";
 			}
-			if(row === 66 || row === 71){
+			if (row === 66 || row === 71) {
 				pieceClasses = pieceClasses + "knight";
 			}
-			if(row === 67 || row === 70){
+			if (row === 67 || row === 70) {
 				pieceClasses = pieceClasses + "bishop";
 			}
-			if(row === 68){
+			if (row === 68) {
 				pieceClasses = pieceClasses + "king";
 			}
-			if(row === 69){
+			if (row === 69) {
 				pieceClasses = pieceClasses + "queen";
 			}
-			pieceClasses = (col === 8)?(pieceClasses + " white"):(pieceClasses + " black");
+			pieceClasses = (col === 8) ? (pieceClasses + " white") : (pieceClasses + " black");
 			icon.setAttribute('class', pieceClasses);
 			piece.appendChild(icon)
-			
+
 		}
-		if(col === 2 || col === 7){
+		if (col === 2 || col === 7) {
 			pieceClasses = pieceClasses + "pawn";
-			pieceClasses = (col === 7)?(pieceClasses + " white"):(pieceClasses + " black");
+			pieceClasses = (col === 7) ? (pieceClasses + " white") : (pieceClasses + " black");
 			icon.setAttribute('class', pieceClasses);
 			piece.appendChild(icon)
 		}
-		
+
 		tile.id = letter + col;
 		tile.title = letter + col;
 		tile.setAttribute('class', addedClass);
 		tile.appendChild(piece)
+		tile.addEventListener("click", handleTileClicked);
 		board.appendChild(tile)
 
 		row++;
@@ -65,24 +70,83 @@
 
 	var q = document.getElementsByClassName("piece");
 	var defaultSize = "30px";
-	function handleTileClicked(e) {
-		for (var i = 0; i < q.length; i++) {
-			if (q[i].style.fontSize !== defaultSize) {
-				q[i].style.fontSize = defaultSize;
-				if(q[i].childNodes[0]){
-					q[i].childNodes[0].style.color = i > 16?"black":"white";
-				}
-			}
-		}
-		e.target.style.color = "blue"
-		var content = e.target.parentElement
-		
-		//content = content || e.target;
-		if (content) {
-			console.dir(content)
-			content.style.fontSize = '45px';
-		}
-		
+
+	var selectedPiece = null;
+	var pieceCss = {
+
 
 	}
+
+	function handleTileClicked(e) {
+		console.log("TITLE ", e)
+		switch (e.target.nodeName) {
+			case "DIV":
+				if (selectedPiece) {
+					$(selectedPiece).css(pieceCss);
+					$(e.target).children('.piece').append(selectedPiece)
+					$(selectedPiece).animate({"font-size": "2.1em"}).animate({"font-size": "1.3em"})
+					selectedPiece = null;
+				}
+				break;
+			case "SPAN":
+				if (!selectedPiece) {
+					selectedPiece = jQuery(e.target).detach();
+				}
+
+				var content = e.target.parentElement
+					// if (content) {
+					// 	content.style.fontSize = '45px';
+					// }
+
+
+				break;
+			default:
+				console.log("nuffin")
+		}
+	}
+
+	// function applyToAllPieces() {
+	// 	for (var i = 0; i < q.length; i++) {
+	// 		if (q[i].style.fontSize !== defaultSize) {
+	// 			q[i].style.fontSize = defaultSize;
+	// 			if (q[i].childNodes[0]) {
+	// 				q[i].childNodes[0].style.color = i > 16 ? "black" : "white";
+	// 				q[i].childNodes[0].style
+	// 			}
+	// 		}
+	// 	}
+	// }
+
+	// for (var i = 0; i < q.length; i++) {
+	// 				if (q[i].style.fontSize !== defaultSize) {
+	// 					q[i].style.fontSize = defaultSize;
+	// 					if (q[i].childNodes[0]) {
+	// 						q[i].childNodes[0].style.color = i > 16 ? "black" : "white";
+	// 					}
+	// 				}
+	// 			}
+
+	// function handlePieceClicked(e) {
+	// 	console.log("piece ", e)
+	// 	for (var i = 0; i < q.length; i++) {
+	// 		if (q[i].style.fontSize !== defaultSize) {
+	// 			q[i].style.fontSize = defaultSize;
+	// 			if (q[i].childNodes[0]) {
+	// 				q[i].childNodes[0].style.color = i > 16 ? "black" : "white";
+	// 			}
+	// 		}
+	// 	}
+	// 	e.target.style.color = "blue";
+	// 	var content = e.target.parentElement
+
+	// 	//content = content || e.target;
+	// 	if (content) {
+	// 		console.dir(content)
+	// 		content.style.fontSize = '45px';
+	// 	}
+
+	// 	//selectedPiece = jQuery(e.target).detach();
+	// }
+
+
 })()
